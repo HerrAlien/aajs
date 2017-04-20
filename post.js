96eddee2ff70403a0dab2078e45bd3926148def7
agarofi <garone80@gmail.com>
2017-04-20 08:05:02 +0300

    Aberration - earth velocity

--- post.js
+++ post.js
@@ -1,9 +1,19 @@
 
 
-    aajs['EasterCalculate'] = function (year, gregorian) {
-        __ZN9CAAEaster9CalculateElb(0, year, gregorian);
-        return  {"month" : HEAP32[0], "day" : HEAP32[1]};
+    aajs['Easter'] = {
+        Calculate : function (year, gregorian) {
+                        __ZN9CAAEaster9CalculateElb(0, year, gregorian);
+                        return  {"month" : HEAP32[0], "day" : HEAP32[1]};
+                    }
+    };
+    
+    aajs['Aberration'] = {
+        EarthVelocity : function (JD, bHighPrecision) {
+            __ZN13CAAAberration13EarthVelocityEdb(0, JD, bHighPrecision);
+            return {"X" : HEAPF64[0], "Y" : HEAPF64[1], "Z" : HEAPF64[2]};
         }
+        
+    };
 
     return aajs;
 })();
