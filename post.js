3c99b26c29d94e1bfd3e34a62f7321c9b1498ad3
agarofi <garone80@gmail.com>
2017-04-20 08:46:41 +0300

    sepparation ...

--- post.js
+++ post.js
@@ -23,6 +23,21 @@
             return {"X" : HEAPF64[0], "Y" : HEAPF64[1]};
         }
     };
+    
+    aajs['AngularSeparation'] = {
+        Separation : function (Alpha1, Delta1, Alpha2, Delta2){
+            return __ZN20CAAAngularSeparation10SeparationEdddd(Alpha1, Delta1, Alpha2, Delta2);
+        },
+        PositionAngle : function (Alpha1, Delta1, Alpha2, Delta2){
+            return __ZN20CAAAngularSeparation13PositionAngleEdddd(Alpha1, Delta1, Alpha2, Delta2);
+        },
+        DistanceFromGreatArc : function (Alpha1, Delta1, Alpha2, Delta2, Alpha3, Delta3){
+            return __ZN20CAAAngularSeparation20DistanceFromGreatArcEdddddd(Alpha1, Delta1, Alpha2, Delta2, Alpha3, Delta3);
+        },
+        SmallestCircle : function (Alpha1, Delta1, Alpha2, Delta2, Alpha3, Delta3, bType1){
+            return __ZN20CAAAngularSeparation14SmallestCircleEddddddRb(Alpha1, Delta1, Alpha2, Delta2, Alpha3, Delta3, bType1);
+        }
+    };
 
     return aajs;
 })();
