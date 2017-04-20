bef979fc8c90a0f53582d174fa90326de97ff4be
agarofi <garone80@gmail.com>
2017-04-20 18:35:52 +0300

    coordinates transform ...

--- post.js
+++ post.js
@@ -63,6 +63,37 @@ with this program. If not, see <https://www.gnu.org/licenses/agpl.html>. */
             ApparentEccentricity : function (e, i, w) {
                 return __ZN13CAABinaryStar20ApparentEccentricityEddd(e, i, w);
             }
+        },
+        CoordinateTransformation : {
+            Equatorial2Ecliptic : function (alpha, delta, epsilon) {
+                __ZN27CAACoordinateTransformation19Equatorial2EclipticEddd (0, alpha, delta, epsilon);
+                return { "X" : HEAPF64[0], "Y" : HEAPF64[1] };
+            },
+            
+            Ecliptic2Equatorial: function (lambda, beta, epsilon) {
+                __ZN27CAACoordinateTransformation19Ecliptic2EquatorialEddd (0, lambda, beta, epsilon);
+                return { "X" : HEAPF64[0], "Y" : HEAPF64[1] };
+            },
+            
+            Equatorial2Horizontal : function (localHourAngle, delta, latitude){
+                __ZN27CAACoordinateTransformation21Equatorial2HorizontalEddd (0, localHourAngle, delta, latitude);
+                return { "X" : HEAPF64[0], "Y" : HEAPF64[1] };
+            },
+            
+            Horizontal2Equatorial : function (az, alt, latitude) {
+                __ZN27CAACoordinateTransformation21Horizontal2EquatorialEddd (0, az, alt, latitude);
+                return { "X" : HEAPF64[0], "Y" : HEAPF64[1] };
+            },
+            
+            Equatorial2Galactic : function (alpha, delta) {
+                __ZN27CAACoordinateTransformation19Equatorial2GalacticEdd (0, alpha, delta);
+                return { "X" : HEAPF64[0], "Y" : HEAPF64[1] };
+            },
+            
+            Galactic2Equatorial : function (l, b) {
+                __ZN27CAACoordinateTransformation19Galactic2EquatorialEdd (0, l, b);
+                return { "X" : HEAPF64[0], "Y" : HEAPF64[1] };
+            }
         }
     };
 })();
