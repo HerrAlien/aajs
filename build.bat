a64884867f8362ce3bc08b779659c21587df77c7
agarofi <garone80@gmail.com>
2017-04-20 09:23:15 +0300

    a few tweaks on the build.

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ for /F %%G in ('dir /b AA*.cpp') do SET A="!A! %%G"
 
 set FILES=%A:"=%
 
-em++ %FILES% %* --pre-js build/pre.js --post-js build/post.js -s LINKABLE=1 -s EXPORT_ALL=1
+em++ --pre-js build/pre.js --post-js build/post.js -s LINKABLE=1 -s EXPORT_ALL=1 -s USE_CLOSURE_COMPILER=1 -s NO_FILESYSTEM=1 %FILES% %*
